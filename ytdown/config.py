3e68e5dbf61aa9d17d8ac3bb8c1f03124e2b74ad
gundoganm <31245314+gundoganmt@users.noreply.github.com>
2022-01-18 21:56:58 +0300

    Update config.py

--- ytdown/config.py
+++ ytdown/config.py
@@ -2,13 +2,7 @@ import psycopg2, os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = '7c6b7967-dcba-4796-a261-f36b028144e3'
+    SECRET_KEY = '7c6b7967-dcba-4796-a261-f36b028144e3' # random secret key
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:WulIgtM5zk@localhost/ytdown"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    MAIL_SERVER = "smtp.zoho.com"
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = "support@pshort.net"
-    MAIL_PASSWORD = "2U8IJi6qI4H2"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ytdown.db') #"postgresql+psycopg2://postgres:password@localhost/ytdown"
+    
