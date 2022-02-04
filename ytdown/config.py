e1b17f889c5836fcc0ff0028eabce468b2548168
gundoganm <31245314+gundoganmt@users.noreply.github.com>
2022-02-04 15:31:19 +0300

    Update config.py

--- ytdown/config.py
+++ ytdown/config.py
@@ -1,7 +1,11 @@
 import psycopg2, os
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 class Config:
     SECRET_KEY = '7c6b7967-dcba-4796-a261-f36b028144e3' # random secret key
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = uri
     
