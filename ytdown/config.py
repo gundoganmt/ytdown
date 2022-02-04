4492666ad1f798354ad114b91f6e33a19fbcf312
gundoganm <31245314+gundoganmt@users.noreply.github.com>
2022-02-04 15:08:07 +0300

    Update config.py

--- ytdown/config.py
+++ ytdown/config.py
@@ -1,8 +1,7 @@
 import psycopg2, os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = '7c6b7967-dcba-4796-a261-f36b028144e3' # random secret key
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ytdown.db') #"postgresql+psycopg2://postgres:password@localhost/ytdown"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
